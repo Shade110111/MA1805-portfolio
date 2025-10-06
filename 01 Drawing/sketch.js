15a63cf2d9409de960fc1834bfeee6c1e48ff6d3
Shade Rickard <edmond.rickard@gmail.com>
2025-10-06 17:06:06 +0100

    fixed stuff

--- 01 Drawing/sketch.js
+++ 01 Drawing/sketch.js
@@ -1,7 +1,26 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
-  background(220);
+  background(205);
+
+  //head
+head_x=400
+head_y=400
+strokeWeight(0)
+fill(215,175,195)
+ellipse(head_x, head_y,170,260);
+fill(215,175,195)
+ellipse(head_x, head_y,170,260);
+
+  //headset
+headset_x=350
+headset_y=350
+strokeWeight(0)
+fill(20,120,160)
+rect(headset_x,headset_y,100,100);
+circle(headset_x,headset_y+50,100);
+circle(headset_x+100,headset_y+50,100);
+
 }
