66605473aba3749ff3eb5a250d0d06346ded80a9
Shade Rickard <edmond.rickard@gmail.com>
2025-10-13 17:42:47 +0100

    working pillars

--- 02 Variables/sketch.js
+++ 02 Variables/sketch.js
@@ -1,15 +1,18 @@
 function setup() {
   createCanvas(800, 800);
   let x;
-
   strokeWeight(0);
+
+  textAlign(CENTER);
+  textSize(20);
+  text('Click me to explore the city!',400,400);
 }
 
 function mouseClicked() {
   background(50+random(100));
   x = random(20,60);
   for (let i=0;i<x;i++){
-    fill(50+random(100));
+    fill(random(60,130));
 
     //buildings
     let x=random(0,width);
@@ -23,10 +26,28 @@ function mouseClicked() {
     //buildings
     rect(x,y,w,h);
 
+    //pillars
+    let pillarWidth = 7;
+    if (random(0,1)>0.8){
+      let pillar_x = random(x+5,x+w-5)-pillarWidth/2;
+      let pillar_y = y+h
+      rect(pillar_x,pillar_y, pillarWidth, 1000)
+      //flip x for other pillar
+      pillar_x=(w-(pillar_x-x))+x-pillarWidth
+      rect(pillar_x,pillar_y, pillarWidth, 1000)
+    }
+
     //plant pots
     let pot_x = random(x,w+x)-7/2;
-    let pot_y = y-10
+    let pot_y = y-9
     fill(200,100,100);
-    rect(pot_x,pot_y,7,10);
+    rect(pot_x,pot_y,7,9);
+    fill(100,50,50);
+    rect(pot_x+2,pot_y-9,3,9);
+    fill(random(90,130),random(160,190),100);
+    rect(pot_x+1,pot_y-13,5,random(7,11));
+
+    
+
   }
 }
