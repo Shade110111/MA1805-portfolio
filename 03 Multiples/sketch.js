c42ba5871921af677df7adabd701a332c4c9c73d
Shade Rickard <edmond.rickard@gmail.com>
2025-10-21 09:25:19 +0100

    fixed perspective issues

--- 03 Multiples/sketch.js
+++ 03 Multiples/sketch.js
@@ -1,5 +1,4 @@
 let time = 0;
-let frame = 0
 let y=0;
 let x=0;
 
@@ -10,27 +9,26 @@ function setup() {
 }
 
 function draw() {
-  frame += 1
   background(70);
 
   time = time + 1/60;
   fill(240,0,0)
   text("Time: " + time.toFixed(0), windowWidth/2, 40);
 
-  for(let y_=80; y < windowHeight; y_ += 50){
-    if y_ < 500 {
-      y = 50*Math.exp(y_/500);
-    } 
-    else {
-      y = y;
+  incriment = 1 //for perspective effect
+  for(let y=100; y < windowHeight; y += incriment){
+    if (incriment < 50){
+      incriment += 1 //for perspective effect
     }
     
     for(let x=0; x<windowWidth; x+= 50){
       fill(160,130,120);
-      strokeWeight(5);
+      strokeWeight(4);
       stroke(100,70,60);
       square(x,y,50);
       strokeWeight(0);
+      
     }
+
   }
 }
