5a1d9f9384fd9de80af3bb5b2a6e3b3e66bc92e9
Shade Rickard <edmond.rickard@gmail.com>
2025-10-27 13:52:14 +0000

    started project 4 media

---
+++ 04 Media/sketch.js
@@ -0,0 +1,12 @@
+
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  let window_x = windowWidth*0.75;
+  let window_y = windowHeight*0.25;
+}
+
+function draw() {
+  background(80);
+  
+}
