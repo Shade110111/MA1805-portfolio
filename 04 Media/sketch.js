d438b54213adbe26d76fb89a42d790f2ac991b92
Shade Rickard <edmond.rickard@gmail.com>
2025-10-27 14:13:47 +0000

    basic environment working without images

--- 04 Media/sketch.js
+++ 04 Media/sketch.js
@@ -2,11 +2,15 @@
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  let window_x = windowWidth*0.75;
-  let window_y = windowHeight*0.25;
+  window_width = 300;
+  window_height = 400;
+  window_x = (windowWidth*0.75) - window_width/2;
+  window_y = (windowHeight*0.25) - window_height/2;
 }
 
 function draw() {
   background(80);
-  
+  fill(250,110,80);
+  strokeWeight(0)
+  rect(window_x,window_y,window_width,window_height,2,2,2,2);
 }
